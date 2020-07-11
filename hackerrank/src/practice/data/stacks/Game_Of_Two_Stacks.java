f9bbc7a4560405873152d55443863670c39bcee8
2pm <>
2020-07-11 15:53:25 +0900

    add solution (game of two stacks)

--- hackerrank/src/practice/data/stacks/Game_Of_Two_Stacks.java
+++ hackerrank/src/practice/data/stacks/Game_Of_Two_Stacks.java
@@ -1,9 +1,8 @@
 package practice.data.stacks;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Stack;
@@ -18,62 +17,88 @@ public class Game_Of_Two_Stacks {
          * Write your code here.
          */
 
-    	Stack<Integer> stack1 = new Stack<Integer>();
-    	Stack<Integer> stack2 = new Stack<Integer>();
-    	
-    	Stack<Integer> sumA = new Stack<Integer>();
-    	Stack<Integer> sumB = new Stack<Integer>();
+    	Stack<Integer> stackA = new Stack<Integer>();
+    	Stack<Integer> stackB = new Stack<Integer>();
     	
     	for(int i=a.length-1; i>=0; i--) {
-    		stack1.push(a[i]);
+    		stackA.push(a[i]);
     	}
     	for(int i=b.length-1; i>=0; i--) {
-    		stack2.push(b[i]);
+    		stackB.push(b[i]);
     	}
     	
-//    	int sum = 0;
-//    	while(!stack1.empty() && sum > x) {
-//    		int ele = stack1.pop();
-//    		sumA.push(ele);
-//    		sum += ele;
-//    	}
-//    	
-//    	while(!stack2.empty()) {
-//    		int ele = stack2.pop();
-//    		sumB.push(ele);
-//    		sum += ele;
-//    		
-//    		while(sum > x) {
-//    			sumA.pop();
-//    		}
-//    		
-//    	}
+//    	System.out.println("stackA : " + stackA.toString());
+//    	System.out.println("stackB : " + stackB.toString());
     	
     	int sum = 0;
-    	int cnt = 0;
-    	while(sum <= x) {
-    		
-    		int s1 = !stack1.empty() ? stack1.peek() : Integer.MAX_VALUE;
-    		int s2 = !stack2.empty() ? stack2.peek() : Integer.MAX_VALUE;
-    		
-    		if(s1 < s2) {
-    			sum += stack1.pop();
-    		}
-    		else if(s1 > s2){
-    			sum += stack2.pop();
-    		}
-    		else if(s1 == s2 && s1 != Integer.MAX_VALUE) {
-    			sum += stack1.pop();
-    		}
-    		else {
-    			break;
-    		}
-    		cnt++;
+    	ArrayDeque<Integer> queA = new ArrayDeque<Integer>();
+    	
+    	// Sum에 Stack A에 대해서만 우선 반영한다. 
+    	for(int i=0; i<a.length; i++) {
+			int val = stackA.pop();
+			if(sum + val > x)
+				break;
+			
+			sum += val;			// sum 안에 들어간 a 의 합 
+			queA.offer(val);	// sum 안에 반영된 a 개수와 값 
     	}
     	
-    	if(sum > x)
-    		--cnt;
+//    	System.out.println("stackA : " + stackA.toString());
+//    	System.out.println("stackB : " + stackB.toString());
+//    	System.out.println("queA : " + queA.toString());
+    	
+    	// Sum에 Stack A 원소의 개수 
+    	int cnt = queA.size();
+    	
+    	// Sum에 Stack B에 대한 내용을 추가 반영한다. (단, 기존 Stack A로 반영된 값을 일부 제거한다.)
+    	for(int i=0; i<b.length; i++) {
+//    			System.out.println("[0] i: " + i + ", x: " + x + ", sum: " + sum + ", Q: " + queA.toString() + ", b: " + stackB.peek() + ", cnt: " + cnt);
+    			
+    			// sum 안에 b값 1개 추가 (sum값이 x값을 넘어가는 시점) 
+    			sum += stackB.pop();			
+    			
+    			// sum 안에 추가된 b값만큼 a값 제거 (sum값이 x값 아래로 내려가는 시점) 
+    			while(sum > x && !queA.isEmpty()) {		
+    				sum -= queA.pollLast();
+    			}
+    			
+    			// (충족된) sum을 구성하는 a와 b에 대한 갯수 갱신
+    			int cnt_tmp = queA.size() + (i+1);
+    			if(sum <= x && cnt_tmp > cnt) {
+    				cnt = cnt_tmp;
+    			}
+    			
+//    			System.out.println("[+] i: " + i + ", x: " + x + ", sum: " + sum + ", Q: " + queA.toString() + ", b: " + stackB.peek() + ", cnt: " + cnt);
+    	}
+    	
     	return cnt;
+    	
+//		잘못된 접근방식 
+//    	int sum = 0;
+//    	int cnt = 0;
+//    	while(sum <= x) {
+//    		
+//    		int s1 = !stack1.empty() ? stack1.peek() : Integer.MAX_VALUE;
+//    		int s2 = !stack2.empty() ? stack2.peek() : Integer.MAX_VALUE;
+//    		
+//    		if(s1 < s2) {
+//    			sum += stack1.pop();
+//    		}
+//    		else if(s1 > s2){
+//    			sum += stack2.pop();
+//    		}
+//    		else if(s1 == s2 && s1 != Integer.MAX_VALUE) {
+//    			sum += stack1.pop();
+//    		}
+//    		else {
+//    			break;
+//    		}
+//    		cnt++;
+//    	}
+//    	
+//    	if(sum > x)
+//    		--cnt;
+//    	return cnt;
     }
     
 
@@ -83,7 +108,7 @@ public class Game_Of_Two_Stacks {
 		Scanner scanner = null;
 		
 		// output
-		File f2 = new File("../Game_Of_Two_Stacks_output01.txt");
+		File f2 = new File("/Users/dean/git/lec_alg/hackerrank/src/practice/data/stacks/Game_Of_Two_Stacks_output01.txt");
 		try {
 	    	scanner = new Scanner(f2);
 	    } catch (Exception e) {
@@ -98,7 +123,7 @@ public class Game_Of_Two_Stacks {
 		}
 		
 		// input
-		File f = new File("../Game_Of_Two_Stacks_input01.txt");
+		File f = new File("/Users/dean/git/lec_alg/hackerrank/src/practice/data/stacks/Game_Of_Two_Stacks_input01.txt");
 	    
 	    try {
 	    	scanner = new Scanner(f);
@@ -139,7 +164,7 @@ public class Game_Of_Two_Stacks {
 
             int result = twoStacks(x, a, b);
             if(outList.get(gItr) != result) {
-            	System.out.println("Error : " + gItr);
+            	System.out.println("Error i : " + gItr + ", Expect : " + outList.get(gItr) + ", Real : " + result);
             }
             
 //            bufferedWriter.write(String.valueOf(result));
